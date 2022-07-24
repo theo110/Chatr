6c900ccc74acc286b2050e85932a3c520b8b14db
Theo Roh <shouyuanroh@gmail.com>
2022-07-24 18:25:31 -0400

    minor

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require("path")
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
 app.use(express.static('client/dist'))
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
